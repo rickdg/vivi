9cb299e228fee34a69f5e8bc7fe6bbb2c35bc80f
Andreas Wieczorreck <andreas.wieczorreck@zeit.de>
2020-02-13 09:25:16 +0100

    Add dogpile.cache

--- core/src/zeit/connector/firestore.py
+++ core/src/zeit/connector/firestore.py
@@ -6,8 +6,13 @@ from six.moves.urllib.parse import urlparse
 from google.cloud import datastore
 from interfaces import IConnector
 from io import BytesIO
-
 import zeit.connector.resource
+
+from dogpile.cache import make_region
+region = make_region().configure(
+    'memory',
+    expiration_time=3600
+)
 
 
 @zope.interface.implementer(zeit.connector.interfaces.IConnector)
@@ -26,6 +31,7 @@ class Firestore(object):
             last = last + '/'
         return parent + '/', last
 
+    @region.cache_on_arguments()
     def __getitem__(self, id):
         query = self.client.query(kind=self.entity_type)
         query.add_filter('metadata.meta.url', '=', urlparse(id).path)
@@ -36,12 +42,34 @@ class Firestore(object):
 
         result = result[0]
 
-        return zeit.connector.resource.Resource(
-            six.text_type(id), self._id_splitlast(id)[1].rstrip('/'),
-            result['metadata']['meta']['type'],
-            BytesIO(('<article><head/>%s</article>' % result['body']).encode('utf-8')),
-            self._get_properties(result['metadata']),
-            '')
+        _type = result['metadata']['meta']['type']
+        url = result['metadata']['meta']['url']
+        print(url)
+
+        if _type == 'article':
+            article = zeit.connector.resource.Resource(
+                six.text_type(id), self._id_splitlast(id)[1].rstrip('/'),
+                _type,
+                BytesIO(('<article><head/>%s</article>' % result['body']).encode('utf-8')),
+                self._get_properties(result['metadata']),
+                '')
+            return article
+        elif _type == 'link':
+            link = zeit.connector.resource.Resource(
+                six.text_type(id), self._id_splitlast(id)[1].rstrip('/'),
+                _type,
+                BytesIO(result['body'].encode('utf-8')),
+                self._get_properties(result['metadata']),
+                '')
+            return link
+        else:
+            obj = zeit.connector.resource.Resource(
+                six.text_type(id), self._id_splitlast(id)[1].rstrip('/'),
+                _type,
+                BytesIO(result['body'].encode('utf-8')),
+                self._get_properties(result['metadata']),
+                '')
+            return obj
 
     def _get_properties(self, metadata):
         result = {}
